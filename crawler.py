f773e367175936bd6e72f3e99a0ee1dee1b2ebb5
szhmery <szhmery@gmail.com>
2018-05-14 22:58:45 +0800

    first commit

---
+++ crawler.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://jr.jd.com')
+bs_obj = BeautifulSoup(html.read(), 'html.parser')
+text_list = bs_obj.find_all("a","nav-item-primary")
+for text in text_list:
+    print(text.get_text())
+print(html.read())
+html.close()
